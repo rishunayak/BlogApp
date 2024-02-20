669abf3ccbfdc20906c45291adb33aa611b4e319
rishunayak <rishunayak22@gmail.com>
2024-02-20 18:19:34 +0530

    created pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {Route, Routes} from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/Signin'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+
 
 const App = () => {
   return (
-    <div>App hello</div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+
+      </Routes>
+    </>
   )
 }
 
