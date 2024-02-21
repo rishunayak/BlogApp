6646fa40096e0b06bc0ad9a4b988cfb5a0ac4792
rishunayak <rishunayak22@gmail.com>
2024-02-21 16:56:34 +0530

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 import express from 'express'
+import dotenv from "dotenv"
+import connectToMongoDb from './db/connectToMongoDb';
 
-const app = express()
 
+const app = express();
+
+dotenv.config();
 
 
 app.link(8080,()=>
 {
+    connectToMongoDb()
     console.log("server is runing on server 8080")
 })
