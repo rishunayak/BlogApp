78fd3fd97afcad76299e3e1a8bc60f22908f8b4f
rishunayak <rishunayak22@gmail.com>
2024-02-22 12:06:49 +0530

    created sigin Api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 
 
@@ -48,5 +49,39 @@ export const signup=async(req,res)=>
 
 export const login=async(req,res)=>
 {
+    try {
+        const {username,password}=req.body
+        const user=await User.findOne({username})
+        const isPasswordCorrect= await bcrypt.compare(password,user?.password || "");
+          
+          if(!user || !isPasswordCorrect)
+          {
+            return res.status(400).json({error:"Invalid username or password"})
+          }
+    
+          generateTokenAndsetCookie(user._id,res);
+    
+          res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            username:user.username,
+            profilePic:user.profilePic
+          })
+    
+      
+       } catch (error) {
+          console.log("Error in login Controller",error.message)
+          res.status(500).json({error:"Internal Server Error"})
+       }
+}
 
+
+const generateTokenAndsetCookie=(userId,res)=>
+{
+    const token=jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"15d"})
+    res.cookie("jwt",token,{
+        maxAge:15*24*60*60*1000,
+        httpOnly:true,  // save from XSS attacks crross site 
+        sameSite:"strict", // CSRF attack cross site 
+    })
 }
