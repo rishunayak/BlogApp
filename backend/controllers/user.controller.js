f7672e80471e4f64400704339dbd86beac2bc1a8
rishunayak <rishunayak22@gmail.com>
2024-02-22 20:45:42 +0530

    created user update api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,1 +1,51 @@
+import bcrypt from "bcryptjs"
+import User from "../models/user.model.js";
 
+export const updateUser=async(req,res)=>
+{
+    if(req.user._id !== req.params.userId)
+    {
+        return res.status(403).json({error:'You are not allowed to updare this user'});
+    }
+    if(req.body.password)
+    {
+        if(req.body.password.length<6)
+        {
+            return res.status(400).json({error:'Password must be atleast 6 characters'});
+        }
+        if(req.body.username.trim().length <6 ||req.body.username.trim().length >15 )
+        {
+            return res.status(400).json({error:'Username must be 6 to 15 characters'});
+        }
+        if(req.body.username.includes(' '))
+        {
+            return res.status(400).json({error:'Username cannot contain spaces'});
+        }
+
+        if(!req.body.username.match(/^[a-zA-Z0-9]+$/))
+        {
+            return res.status(400).json({error:'Username can only contain letters and numbers'});
+        }
+
+        const salt=await bcrypt.genSalt(10);
+        const hashedPassword=await bcrypt.hash(req.body.password,salt)
+        
+        try {
+
+            const updatedUser=await User.findByIdAndUpdate(req.params.userId,{$set:
+                {
+                    username:req.body.username,
+                    profilePicture:req.body.profilePicture,
+                    password:hashedPassword
+                }},{new:true})
+
+                const {password,...rest}=updateUser._doc;
+                res.status(200).json(rest);
+            
+        } catch (error) {
+            console.log("Error in singup Controller",error.message)
+            res.status(500).json({error:"Internal Server Error"})
+        }
+
+    }
+}
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,11 @@
 import express from "express"
+import { updateUser } from "../controllers/user.controller.js";
+import { auth } from "../middleware/auth.js";
 
 const router =express.Router();
 
 router.get("/test",)
+router.put('/update/:userId',auth,updateUser);
 
 
 export default router; 
