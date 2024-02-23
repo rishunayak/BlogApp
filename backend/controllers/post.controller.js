bd27dbf1d497450a2abf97e9d4237947f723d821
rishunayak <rishunayak22@gmail.com>
2024-02-23 17:54:56 +0530

    created get posts api routes

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -19,7 +19,43 @@ export const create=async(req,res)=>
         const savePost=await newPost.save();
         res.status(201).json(savePost)
     } catch (error) {
-        console.log("Error in singup Controller",error.message)
+        console.log("Error in Post Controller",error.message)
         res.status(500).json({error:"Internal Server Error"})
     }
+}
+
+export const getPosts=async(req,res)=>
+{
+  try {
+     const startIndex=+(req.query.startIndex) || 0;
+     const limit=+(req.query.startIndex) || 9;
+     const sortDirection=req.query.order==='asc'?1:-1;
+     const posts=await Post.find({
+        ...(req.query.userId && {userId:req.query.userId}),
+        ...(req.query.category && {category:req.query.category}),
+        ...(req.query.slug && {slug:req.query.slug}),
+        ...(req.query.postId && {_id:req.query.postId}),
+        ...(req.query.searchTerm && {
+            $or:[
+                {title:{$regex:req.query.searchTerm,$options:'i'}},
+                {content:{$regex:req.query.searchTerm,$options:'i'}}
+            ]
+        } )
+     }).sort({updatedAt:sortDirection}).skip(startIndex).limit(limit)
+     
+     const totalPosts=await Post.countDocuments();
+     const now =new Date();
+     const oneMonthAgo=new Date(now.getFullYear(),
+     now.getMonth()-1,
+     now.getDate())
+
+     const lastMonthPosts=await Post.countDocuments({
+        createdAt:{$gte:oneMonthAgo},
+     })
+     res.status(200).json({posts,totalPosts,lastMonthPosts})
+ 
+    } catch (error) {
+    console.log("Error in Post Controller",error.message)
+    res.status(500).json({error:"Internal Server Error"})
+  }
 }
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { auth } from "../middleware/auth.js";
-import { create } from "../controllers/post.controller.js";
+import { create, getPosts } from "../controllers/post.controller.js";
 
 const router=express.Router();
 
-router.post('/create',auth,create)
+router.post('/create',auth,create);
+router.get('/getPost',getPosts);
 
 export default router;
