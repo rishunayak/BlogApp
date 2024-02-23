026f772b9cbb781045c31941abeda9b9857b6cd5
rishunayak <rishunayak22@gmail.com>
2024-02-23 17:28:25 +0530

    added posts section to the dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import DashSidebar from '../components/DashSidebar'
 import DashProfile from '../components/DashProfile'
+import DashPost from '../components/DashPost'
 
 const Dashboard = () => {
 
@@ -26,6 +27,7 @@ const Dashboard = () => {
         <DashSidebar/>
       </div>
       {tab ==='profile' && <DashProfile/>}
+      {tab==='posts' && <DashPost/>}
     </div>
   )
 }
