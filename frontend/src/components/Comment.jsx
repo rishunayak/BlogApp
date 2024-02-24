13417c8f1c72cab23184523e20a3df16675260e1
rishunayak <rishunayak22@gmail.com>
2024-02-24 17:50:49 +0530

    added like functionality to comment of the post

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import moment from "moment"
-const Comment = ({comment}) => {
+import { FaThumbsUp } from 'react-icons/fa'
+import { useSelector } from 'react-redux'
+const Comment = ({comment,onLike}) => {
+   const {currentUser}=useSelector((store)=>store.user)
+
   return (
-    <div className='flex p-4 border-b dark:border-gray-600 text-sm'>
+    <div className='flex p-4 border-b dark:border-gray-600 text-sm' >
         <div className='flex-shrink-0 mr-3'>
-            <img className='w-10 h-10 rounded-full bg-gray-200' src={comment.userId.profilePicture} alt={comment.userId.username} />
+            <img className='w-10 h-10 rounded-full bg-gray-200' src={comment?.userId?.profilePicture} alt={comment?.userId?.username} />
         </div>
         <div className='flex-1'>
             <div className='flex items-center mb-1'>
                 <span className=' font-bold mr-1 text-xs truncate'>
-                    {comment.userId.username?`@${comment.userId.username}`:"anonymos user"}
+                    {comment?.userId?.username?`@${comment?.userId?.username}`:"anonymos user"}
                 </span>
-                <span className='text-gray-500 text-xs'>{moment(comment.createdAt).fromNow()}</span>
+                <span className='text-gray-500 text-xs'>{moment(comment?.createdAt).fromNow()}</span>
             </div>
-            <p className='text-gray-500 pb-2'>{comment.content}</p>
+            <p className='text-gray-500 pb-2'>{comment?.content}</p>
+            <div className='flex gap-2 text-xs'>
+                <button type='button' onClick={()=>onLike(comment?._id)} className={`text-gray-400 hover:text-blue-500 
+                ${currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500'}`}> 
+                    <FaThumbsUp className='text-sm'/>
+                </button>
+                <p className='text-gray-400'>
+                    {comment.numberOfLikes>0 && comment.numberOfLikes +" "+
+                    (comment.numberOfLikes===1?"like":'likes')}
+                </p>
+            </div>
         </div>
     </div>
   )
--- frontend/src/components/CommentSection.jsx
+++ frontend/src/components/CommentSection.jsx
@@ -1,7 +1,7 @@
 import { Alert, Button, TextInput, Textarea } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Comment from './Comment';
 
 const CommentSection = ({postId}) => {
@@ -10,6 +10,7 @@ const CommentSection = ({postId}) => {
     const [loading,setLoading]=useState(null)
     const [commentError,setCommentError]=useState(null)
     const [comments,setComments]=useState([]);
+    const nav=useNavigate();
     const handleSubmit=async(e)=>
     {
        e.preventDefault()
@@ -65,7 +66,40 @@ const CommentSection = ({postId}) => {
        }
        getComments();
     },[])
-    console.log(comments)
+    
+
+    const handleLike=async(commentId)=>
+    {
+      
+       try {
+        if(!currentUser)
+        {
+            nav("/sign-in")
+            return;
+        }
+
+        const res=await fetch(`/api/comment/likeComment/${commentId}`,
+        {
+            method:'PUT',
+        })
+
+        if(res.ok)
+        {
+           
+            const data=await res.json();
+           const newComments=comments.map((comment)=>
+           {
+
+              return  comment._id==commentId?{...comment,likes:data.likes,
+                numberOfLikes:data.likes.length}:comment;
+           })
+            setComments(newComments)
+        }
+
+       } catch (error) {
+        
+       }
+    }
 
   return (
     <div className='max-2-xl mx-auto w-full p-3'>
@@ -110,10 +144,10 @@ const CommentSection = ({postId}) => {
             <div className='text-sm my-5 flex items-center gap-1'>
                 <p>Comments</p>
                  <div className='border border-gray-400 py-1 px-2 rounded-sm'>
-                    <p>{comments.length}</p>
+                    <p>{comments?.length}</p>
                  </div>
             </div>
-             {comments.map(comment=> <Comment key={comment._id} comment={comment}/>)}
+             {comments?.map(comment=> <Comment key={comment?._id} onLike={handleLike}  comment={comment}/>)}
             </>
          )}
     </div>
