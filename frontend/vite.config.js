1f9a3269dc4087260710f3fc4ec9deb3b6a0e3e3
rishunayak <rishunayak22@gmail.com>
2024-02-25 20:16:26 +0530

    updated api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server:{
     port:3000,
-    proxy:{"/api":{target:"http://localhost:8080",secure:false}}
+    proxy:{"/api":{target:"https://blogapp-6vnt.onrender.com",secure:false}}
   },
   plugins: [react()],
 })
