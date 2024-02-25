4fa5e72daae9f550e6535b6025fb0070e5a55585
rishunayak <rishunayak22@gmail.com>
2024-02-25 20:10:00 +0530

    added pre installation for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser';
 import postRoutes from "./routes/post.route.js"
 import commentRoutes from "./routes/comment.route.js"
 
+const __dirname=path.resolve();
 const app = express();
 
 dotenv.config();
@@ -18,6 +19,12 @@ app.use("/api/user",userRoutes);
 app.use("/api/auth",authRoutes);
 app.use('/api/post',postRoutes);
 app.use('/api/comment',commentRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*",(req,res)=>
+{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 
 app.listen(8080,()=>
