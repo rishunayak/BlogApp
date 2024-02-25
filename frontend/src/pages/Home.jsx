d2e681679914f55a3fd1dacf28e52c3ebc0a6e9a
rishunayak <rishunayak22@gmail.com>
2024-02-25 13:20:50 +0530

    completed home page ui and functionality

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import PostCard from '../components/PostCard';
 
 const Home = () => {
+  const [posts,setPosts]=useState([]);
+
+  useEffect(()=>
+  {
+    const fetchPosts=async()=>
+       {
+        try {
+            const res=await fetch('/api/post/getPost')
+            const data=await res.json();
+            if(res.ok)
+            {
+             setPosts(data.posts)
+            }
+           } catch (error) {
+             console.log(error.message)
+           }
+       }
+       fetchPosts();
+  },[])
   return (
-    <div>Home</div>
+    <div>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
+        <p className='text-gray-500 text-sm sm:text-sm'>Here you'll find a veriety of articles and tutorials on topics such as web development,
+          software enginnering, and programming languages.
+        </p>
+        <Link  to={"/search"} className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'>View all posts</Link>
+      </div>
+      
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+       {
+         posts && posts.length>0 && (
+          <div className=''>
+            <h2 className=' text-2xl font-semibold text-center'>Recent Posts</h2>
+            <div className='flex flex-wrap gap-4 mt-10 justify-center'>
+              {
+                posts.map((post)=>(<PostCard key={post._id} post={post}/> ))
+              }
+            </div>
+            <div className='text-center mt-5'>
+            <Link  to={"/search"} className=' text-xs sm:text-sm text-teal-500 font-bold hover:underline'>View all posts</Link>
+            </div>
+            
+          </div>
+         )
+       }
+      </div>
+     
+    </div>
   )
 }
 
